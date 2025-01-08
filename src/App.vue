6f263dd98194da45a342cea568b9e0d8ea581ec2
苏杰豪 <megasu@qq.com>
2025-01-08 17:58:53 +0800

    01-综合案例-列表渲染

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
+import axios from 'axios';
 import Edit from './components/Edit.vue'
+import { onMounted } from 'vue';
+import { ref } from 'vue';
 
 // TODO: 列表渲染
-
+const list = ref([])
+const getList = async () => {
+  const res = await axios.get('/list')
+  // console.log(res);
+  list.value = res.data
+}
+onMounted(() => {
+  getList()
+})
 
 // TODO: 删除功能
 
@@ -13,11 +24,7 @@ import Edit from './components/Edit.vue'
 
 <template>
   <div class="app">
-    <el-table :data="[{
-      id: 1,
-      name: 'jack',
-      place: 'none'
-    }]">
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
