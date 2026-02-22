c33ad5d53dbc7b5705918fb82c98517e9d733541
Wxq <Wxq@123.cn>
2026-02-22 21:14:00 +0800

    完成 Vue3 列表编辑、删除功能，修复接口调用报错

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,51 @@
 <script setup>
 import Edit from './components/Edit.vue'
+import axios from 'axios';
+import { ref, onMounted } from 'vue'
+import { ElMessage } from 'element-plus'
 
 // TODO: 列表渲染
-
+const list = ref([])
+const getList = async () => {
+  const res = await axios.get('/list')
+  list.value = res.data
+}
+onMounted(() => {
+  getList()
+})
 
 // TODO: 删除功能
-
+const del = async (id) => {
+  await axios.delete(`/del/${id}`)
+  // console.log(id);
+  ElMessage.success('删除成功')
+  getList()
+}
 
 // TODO: 编辑功能
+const tableRef = ref()
 
+const edit = (row) => {
+  tableRef.value.open(row) //打开弹窗顺道传个row
+
+}
 </script>
 
 <template>
   <div class="app">
-    <el-table :data="[{
-      id: 1,
-      name: 'jack',
-      place: 'none'
-    }]">
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
-          <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+        <template #default="{ row }">
+          <el-button type="primary" link @click="edit(row)">编辑</el-button>
+          <el-button type="danger" link @click="del(row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref="tableRef" @updata="getList" />
 </template>
 
 <style scoped>
--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,8 +1,31 @@
 <script setup>
 // TODO: 编辑
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { defineEmits } from 'vue'
+import { ElMessage } from 'element-plus'
+
+const emit = defineEmits(['updata'])
+const data = ref({})
 // 弹框开关
 const dialogVisible = ref(false)
+const open = (row) => {
+  dialogVisible.value = true
+  // console.log(row);
+  data.value = { ...row } // 把传来的row记录一下 发请求用
+}
+defineExpose({ open })
+
+// 点确认 更新数据 -> 通知父组件刷新页面
+const edit = async () => {
+  await axios.patch(`/edit/${data.value.id}`,
+    data.value
+  )
+  dialogVisible.value = false
+  emit('updata')
+  ElMessage.success('编辑成功')
+}
+
 
 
 </script>
@@ -11,16 +34,16 @@ const dialogVisible = ref(false)
   <el-dialog v-model="dialogVisible" title="编辑" width="400px">
     <el-form label-width="50px">
       <el-form-item label="姓名">
-        <el-input placeholder="请输入姓名" />
+        <el-input placeholder="请输入姓名" v-model="data.name" />
       </el-form-item>
       <el-form-item label="籍贯">
-        <el-input placeholder="请输入籍贯" />
+        <el-input placeholder="请输入籍贯" v-model="data.place" />
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
+        <el-button type="primary" @click="edit()">确认</el-button>
       </span>
     </template>
   </el-dialog>
